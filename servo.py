4ff7c5db4cb2be9a0dfb1f20a18b4d8790d0dc97
Wor2ke <88055472+Wor2ke@users.noreply.github.com>
2023-04-12 23:59:15 +0300

    commit 1
    Реализация Singleton через  магический метод __new__
    магический метод __del__ нужен для удаления ссылки, если на нее ничего не ссылается

--- servo.py
+++ servo.py
@@ -3,8 +3,17 @@ import math
 import rdrive as rr
 
 class Servo:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __del__(self):
+        Servo.__instance = None
+      
     def __init__(self, name_interface, name_id, logger):
-        print(name_interface)
         logger.info("Initializing ServoApi")
         self.api = rr.ServoApi()
 
