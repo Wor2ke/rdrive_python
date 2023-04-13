f7e65acf015b7b4de54e1d4fca68c739081f333a
Wor2ke <88055472+Wor2ke@users.noreply.github.com>
2023-04-13 20:42:06 +0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,22 +1,12 @@
+import math
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from servo import Servo
 
 
-def clicked_ok():
-    global var
-    koeficient = var.get()
-    print(koeficient)
-
-
 def interface(INTERFACE_NAME, SERVO_1_ID, logger):
-    servo_obj = Servo(INTERFACE_NAME, SERVO_1_ID, logger)
-    servo_obj1 = Servo("Ker", 228, logger)
-
-    if id(servo_obj) == id(servo_obj1):
-        print("Same instance")
-    
+    dec = Servo(INTERFACE_NAME, SERVO_1_ID, logger)
     window = Tk()
     oves = 10.0
     ris = 6.8
@@ -24,54 +14,76 @@ def interface(INTERFACE_NAME, SERVO_1_ID, logger):
     psheno = 3.9
     roj = 16.8
     goroh = 11.7
-    speed = 5
+    speed = 0
+
     temp = 40
     A = 8
-    V = 220
+    V = 47
     var = DoubleVar()
-    
     window.title("БрГТУ")
     window.geometry('800x500')
-    
-    Button(window, text="Старт", bg="blue", fg="white", font=("Arial Bold", 15), command=lambda : servo_obj.start(1, logger), padx=50, pady=50).place(x=60, y=40)
-    Button(window, text="Стоп", bg="blue", fg="white", font=("Arial Bold", 15), command= lambda : servo_obj.stop(logger), padx=50, pady=50).place(x=300, y=40)
-    
-    Label(window, text="Введите необходимый коэфициент:", font=("roboto", 14)).place(x=60, y=220)
-    Entry(window, width=20, textvariable=var).place(x=60, y=270)
-    Button(window, text="Ok", bg="blue", fg="white", font=("Arial Bold", 10), command=clicked_ok, padx=10, pady=10).place(x=190, y=260)
-    Label(window, text="Известные коэфициенты:", font=("roboto", 14)).place(x=500, y=220)
+    btn_start = Button(window, text="Старт", bg="blue", fg="white", font=("Arial Bold", 15), command=lambda :dec.start(3, logger), padx=50, pady=50).place(x=60, y=40)
+    btn_stop = Button(window, text="Стоп", bg="blue", fg="white", font=("Arial Bold", 15), command= lambda :dec.stop(), padx=50, pady=50).place(x=300, y=40)
+    lbl = Label(window, text="Введите необходимый коэфициент:", font=("roboto", 14)).place(x=60, y=220)
+    entry = Entry(window, width=20, textvariable=var).place(x=60, y=270)
+    ok_btn = Button(window, text="Ok", bg="blue", fg="white", font=("Arial Bold", 10), padx=10, pady=10).place(x=190, y=260)
+    lbl_crups = Label(window, text="Известные коэфициенты:", font=("roboto", 14)).place(x=500, y=220)
 
-    Label(window, text="Овес:", font=("roboto", 14)).place(x=500, y=250)
-    Label(window, text=str(oves), font=("roboto", 14)).place(x=660, y=250)
+    lbl_oves = Label(window, text="Овес:", font=("roboto", 14)).place(x=500, y=250)
+    lbl_oves_speed = Label(window, text=str(oves), font=("roboto", 14)).place(x=660, y=250)
 
-    Label(window, text="Рис:", font=("roboto", 14)).place(x=500, y=280)
-    Label(window, text=str(ris), font=("roboto", 14)).place(x=660, y=280)
+    lbl_ris = Label(window, text="Рис:", font=("roboto", 14)).place(x=500, y=280)
+    lbl_ris_speed = Label(window, text=str(ris), font=("roboto", 14)).place(x=660, y=280)
 
-    Label(window, text="Гречка:", font=("roboto", 14)).place(x=500, y=310)
-    Label(window, text=str(grecha), font=("roboto", 14)).place(x=660, y=310)
+    lbl_grecha = Label(window, text="Гречка:", font=("roboto", 14)).place(x=500, y=310)
+    lbl_grecha_speed = Label(window, text=str(grecha), font=("roboto", 14)).place(x=660, y=310)
 
-    Label(window, text="Пшено:", font=("roboto", 14)).place(x=500, y=340)
-    Label(window, text=str(psheno), font=("roboto", 14)).place(x=660, y=340)
+    lbl_psheno = Label(window, text="Пшено:", font=("roboto", 14)).place(x=500, y=340)
+    lbl_psheno_speed = Label(window, text=str(psheno), font=("roboto", 14)).place(x=660, y=340)
 
-    Label(window, text="Рожь:", font=("roboto", 14)).place(x=500, y=370)
-    Label(window, text=str(roj), font=("roboto", 14)).place(x=660, y=370)
+    lbl_roj = Label(window, text="Рожь:", font=("roboto", 14)).place(x=500, y=370)
+    lbl_roj_speed = Label(window, text=str(roj), font=("roboto", 14)).place(x=660, y=370)
 
-    Label(window, text="Горох:", font=("roboto", 14)).place(x=500, y=400)
-    Label(window, text=str(goroh), font=("roboto", 14)).place(x=660, y=400)
+    lbl_goroh = Label(window, text="Горох:", font=("roboto", 14)).place(x=500, y=400)
+    lbl_goroh_speed = Label(window, text=str(goroh), font=("roboto", 14)).place(x=660, y=400)
 
-    Label(window, text="Основные характеристики:", font=("roboto", 14)).place(x=500, y=30)
+    lbl_char = Label(window, text="Основные характеристики:", font=("roboto", 14)).place(x=500, y=30)
 
-    Label(window, text="Скорость(об/мин):", font=("roboto", 14)).place(x=500, y=60)
-    Label(window, text=str(speed), font=("roboto", 14)).place(x=720, y=60)
+    lbl_speed = Label(window, text="Скорость(об/мин):", font=("roboto", 14)).place(x=500, y=60)
+    lbl_speed_char = Label(window, text=str(speed), font=("roboto", 14))
+    lbl_speed_char.place(x=720, y=60)
 
-    Label(window, text="Температура(Гр.):", font=("roboto", 14)).place(x=500, y=90)
-    Label(window, text=str(temp), font=("roboto", 14)).place(x=720, y=90)
+    def update_speed_label():
+        lbl_speed_char.configure(text=f"{abs(dec.get_velocity()):.1f}")
+        window.after(1000, update_speed_label)
+    update_speed_label()
 
-    Label(window, text="Ток(А):", font=("roboto", 14)).place(x=500, y=120)
-    Label(window, text=str(A), font=("roboto", 14)).place(x=720, y=120)
+    lbl_tempriture = Label(window, text="Температура(Гр.):", font=("roboto", 14)).place(x=500, y=90)
+    lbl_temperature_value = Label(window, text=str(temp), font=("roboto", 14))
+    lbl_temperature_value.place(x=720, y=90)
 
-    Label(window, text="Напряжение(В):", font=("roboto", 14)).place(x=500, y=150)
-    Label(window, text=str(V), font=("roboto", 14)).place(x=720, y=150)
+    def update_temperature_label():
+        lbl_temperature_value.configure(text=f"{abs(dec.get_temperature()):.1f}")
+        window.after(1000, update_temperature_label)
+    update_temperature_label()
+
+    lbl_A = Label(window, text="Ток(А):", font=("roboto", 14)).place(x=500, y=120)
+    lbl_ampere_value = Label(window, text=str(A), font=("roboto", 14))
+    lbl_ampere_value.place(x=720, y=120)
+
+    def update_ampere_label():
+        lbl_ampere_value.configure(text=f"{abs(dec.get_amper()):.1f}")
+        window.after(1000, update_ampere_label)
+    update_ampere_label()
+
+    lbl_voltage = Label(window, text="Напряжение(В):", font=("roboto", 14)).place(x=500, y=150)
+    lbl_voltage_value = Label(window, text=str(V), font=("roboto", 14))
+    lbl_voltage_value.place(x=720, y=150)
+
+    def update_voltage_label():
+        lbl_voltage_value.configure(text=f"{abs(dec.get_voltage()):.1f}")
+        window.after(1000, update_voltage_label)
+    update_voltage_label()
 
     window.mainloop()
 
