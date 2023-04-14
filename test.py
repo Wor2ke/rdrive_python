6e4920460a47d6fca8a4d3cde6e34aef2771b3ec
Wor2ke <88055472+Wor2ke@users.noreply.github.com>
2023-04-14 13:03:21 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
 import logging
 import os
 import argparse
+import threading
 import rdrive as rr
 import Interface
+from servo import Servo
+
+logging.basicConfig()
+logger = logging.getLogger(os.path.basename(__file__))
+logger.setLevel(logging.INFO)
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--servo_1_id", type=int, help="first servo ID that you want control"
+)
+parser.add_argument("--interface", type=str, help="interface name")
+print("GO")
+
+args = parser.parse_args()
+
+INTERFACE_NAME = args.interface
+SERVO_1_ID = args.servo_1_id
+
+Interface.interface(INTERFACE_NAME, SERVO_1_ID, logger)
 
 
 
